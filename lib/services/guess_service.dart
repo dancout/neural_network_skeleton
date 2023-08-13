6e8e1091f451b27fee35adbeea2510b4641f8940
Daniel Couturier <dancout@umich.edu>
2023-08-13 11:17:35 -0400

    Create guess_service.dart

---
+++ lib/services/guess_service.dart
@@ -0,0 +1,10 @@
+import 'package:neural_network_skeleton/models/perceptron_layer.dart';
+
+class GuessService {
+  // TODO: Should we make the output be of type <T> so that the user can specify
+  /// whatever they want?
+  List<double> guess({required List<PerceptronLayer> layers}) {
+    // TODO: Implement this function
+    return [1.0];
+  }
+}

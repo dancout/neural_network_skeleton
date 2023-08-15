adbfffb39fdf844e8656cd71ddfb2343efb85675
Daniel Couturier <dancout@umich.edu>
2023-08-14 22:25:29 -0400

    Delete training_service.dart

--- lib/services/training_service.dart
+++
@@ -1,6 +0,0 @@
-import 'package:neural_network_skeleton/models/perceptron_layer.dart';
-
-/// The service responsible for training the Neural Network for improvements.
-abstract class TrainingService {
-  List<PerceptronLayer> train();
-}

9fa81aeec3ae421c6e08831390808ebd916bbfa1
Daniel Couturier <dancout@umich.edu>
2023-08-16 08:51:27 -0400

    MultiLayerPerceptron Documentation

--- lib/models/multi_layer_perceptron.dart
+++ lib/models/multi_layer_perceptron.dart
@@ -1,11 +1,14 @@
 import 'package:equatable/equatable.dart';
 import 'package:neural_network_skeleton/models/perceptron_layer.dart';
 
+/// A collection of [PerceptronLayer] objects meant to be used within a
+/// [NeuralNetwork].
 class MultiLayerPerceptron extends Equatable {
   const MultiLayerPerceptron({
     required this.layers,
   });
 
+  /// The [PerceptronLayer] objects within this [MultiLayerPerceptron].
   final List<PerceptronLayer> layers;
 
   @override
@@ -15,5 +18,7 @@ class MultiLayerPerceptron extends Equatable {
 }
 
 extension MultiLayerPerceptronExtension on MultiLayerPerceptron {
+  /// The number of [PerceptronLayer] objects within this
+  /// [MultiLayerPerceptron].
   int get numLayers => layers.length;
 }

6dcabece60e84c8ea063b2ea7ac73ba55cf2991e
Daniel Couturier <dancout@umich.edu>
2023-08-16 08:57:13 -0400

    Cla,pingOutputNormalizationService documentation

--- lib/services/output_normalization/clamping_output_normalization_service.dart
+++ lib/services/output_normalization/clamping_output_normalization_service.dart
@@ -1,5 +1,7 @@
 import 'package:neural_network_skeleton/services/output_normalization/output_normalization_service.dart';
 
+/// Used to ensure output values for a [Perceptron] remain between 0.0 and 1.0,
+/// inclusively.
 class ClampingOutputNormalizationService extends OutputNormalizationService {
   @override
   normalizeValue({

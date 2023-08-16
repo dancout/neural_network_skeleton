43c0ff0557de5686e7b5ba265a4aadca5897c0e0
Daniel Couturier <dancout@umich.edu>
2023-08-16 08:40:32 -0400

    WeightWeb documentation

--- lib/models/weight_web.dart
+++ lib/models/weight_web.dart
@@ -1,10 +1,17 @@
 import 'package:equatable/equatable.dart';
 
+/// The collection of weights to be multiplied against the collection of
+/// inputs coming into a [Perceptron].
+///
+/// The [NeuralNetwork] is fully-connected, so there must be a weight value for
+/// every [Perceptron] connection from one [PerceptronLayer] to another.
 class WeightWeb extends Equatable {
   const WeightWeb({
     required this.weights,
   });
 
+  /// The list of weights representing how strongly an input will influence the
+  /// target [Perceptron].
   final List<double> weights;
 
   @override

750bb3e5f7af552093b9619d4b198d685c934b6c
Daniel Couturier <dancout@umich.edu>
2023-08-16 08:55:49 -0400

    OutputNormalizationService documentation

--- lib/services/output_normalization/output_normalization_service.dart
+++ lib/services/output_normalization/output_normalization_service.dart
@@ -1,3 +1,9 @@
+/// Used to normalize output data from a [Perceptron].
+///
+/// This may be necessary if your algorithm requires all values to be within a
+/// certain range. Otherwise, values could potentially grow or shrink
+/// disproportionately as they feed forward within the [NeuralNetwork].
 abstract class OutputNormalizationService {
-  normalizeValue({required double value});
+  /// Returns a normalized output [value].
+  double normalizeValue({required double value});
 }

1e4cfe7826ed56129301c3b8280cc63e3adc8040
Daniel Couturier <dancout@umich.edu>
2023-08-16 07:42:50 -0400

    Added tests and setup for XOR problem

--- lib/neural_network_skeleton.dart
+++ lib/neural_network_skeleton.dart
@@ -1,7 +1,161 @@
 library neural_network_skeleton;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:neural_network_skeleton/models/perceptron.dart';
+import 'package:neural_network_skeleton/models/perceptron_layer.dart';
+import 'package:neural_network_skeleton/models/weight_web.dart';
+import 'package:neural_network_skeleton/src/neural_network.dart';
+
+// TODO: Clean this file up!
+
+// The Coding Train example did NOT normalize the weights, and allowed them to be -1 to 1.
+// You can do random() * 2 - 1 to get a random value between -1 and 1
+
+class ExampleNeuralNetwork {
+  List<double> run({
+    required List<double> inputs,
+  }) {
+    final orPerceptron = Perceptron(
+      bias: 0.0,
+      threshold: 1.0,
+      weightWeb: WeightWeb(weights: const [1.0, 1.0]),
+    );
+
+    final notPerceptron = Perceptron(
+      bias: 1.0,
+      threshold: 0.0,
+      weightWeb: WeightWeb(weights: const [0.0, -1.0]),
+    );
+
+    final andPerceptron = Perceptron(
+      bias: 0.0,
+      threshold: 1.0,
+      weightWeb: WeightWeb(
+        weights: const [0.5, 0.5],
+      ),
+    );
+
+    final passthroughPerceptron = Perceptron(
+      bias: 0.0,
+      threshold: 0.0, // Sigmoid prevents 1.0
+      weightWeb: WeightWeb(weights: const [1.0, 0.0]),
+    );
+
+    final layers = [
+      PerceptronLayer(
+        perceptrons: [
+          orPerceptron,
+          andPerceptron,
+        ],
+      ),
+      PerceptronLayer(
+        perceptrons: [
+          passthroughPerceptron,
+          notPerceptron,
+        ],
+      ),
+      PerceptronLayer(
+        perceptrons: [
+          andPerceptron,
+        ],
+      ),
+    ];
+    final neuralNetwork = NeuralNetwork(layers: layers);
+
+    return neuralNetwork.guess(
+      inputs: inputs,
+    );
+  }
+
+  List<double> orTest({
+    required List<double> inputs,
+  }) {
+    final orPerceptron = Perceptron(
+      bias: 0.0,
+      threshold: 1.0,
+      weightWeb: WeightWeb(weights: const [1.0, 1.0]),
+    );
+
+    final layers = [
+      PerceptronLayer(
+        perceptrons: [
+          orPerceptron,
+        ],
+      ),
+    ];
+    final neuralNetwork = NeuralNetwork(layers: layers);
+
+    return neuralNetwork.guess(
+      inputs: inputs,
+    );
+  }
+
+  List<double> andTest({
+    required List<double> inputs,
+  }) {
+    final andPerceptron = Perceptron(
+      bias: 0.0,
+      threshold: 1.0,
+      weightWeb: WeightWeb(
+        weights: const [0.5, 0.5],
+      ),
+    );
+
+    final layers = [
+      PerceptronLayer(
+        perceptrons: [
+          andPerceptron,
+        ],
+      ),
+    ];
+    final neuralNetwork = NeuralNetwork(layers: layers);
+
+    return neuralNetwork.guess(
+      inputs: inputs,
+    );
+  }
+
+  List<double> passthroughTest({
+    required List<double> inputs,
+  }) {
+    final passthroughPerceptron = Perceptron(
+      bias: 0.0,
+      threshold: 0.0, // Sigmoid prevents 1.0
+      weightWeb: WeightWeb(weights: const [1.0, 0.0]),
+    );
+    final layers = [
+      PerceptronLayer(
+        perceptrons: [
+          passthroughPerceptron,
+        ],
+      ),
+    ];
+    final neuralNetwork = NeuralNetwork(layers: layers);
+
+    return neuralNetwork.guess(
+      inputs: inputs,
+    );
+  }
+
+  List<double> notTest({
+    required List<double> inputs,
+  }) {
+    final notPerceptron = Perceptron(
+      bias: 1.0,
+      threshold: 0.0,
+      weightWeb: WeightWeb(weights: const [0.0, -1.0]),
+    );
+
+    final layers = [
+      PerceptronLayer(
+        perceptrons: [
+          notPerceptron,
+        ],
+      ),
+    ];
+    final neuralNetwork = NeuralNetwork(layers: layers);
+
+    return neuralNetwork.guess(
+      inputs: inputs,
+    );
+  }
 }
--- test/neural_network_skeleton_test.dart
+++ test/neural_network_skeleton_test.dart
@@ -1,12 +1,49 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:neural_network_skeleton/neural_network_skeleton.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('xor test', () {
+    final neuralNetwork = ExampleNeuralNetwork();
+
+    expect(neuralNetwork.run(inputs: [0, 0]), [0.0]);
+    expect(neuralNetwork.run(inputs: [1, 0]), [1.0]);
+    expect(neuralNetwork.run(inputs: [0, 1]), [1.0]);
+    expect(neuralNetwork.run(inputs: [1, 1]), [0.0]);
+  });
+
+  test('or test', () async {
+    final neuralNetwork = ExampleNeuralNetwork();
+
+    expect(neuralNetwork.orTest(inputs: [0, 0]), [0.0]);
+    expect(neuralNetwork.orTest(inputs: [1, 0]), [1.0]);
+    expect(neuralNetwork.orTest(inputs: [0, 1]), [1.0]);
+    expect(neuralNetwork.orTest(inputs: [1, 1]), [1.0]);
+  });
+
+  test('and test', () async {
+    final neuralNetwork = ExampleNeuralNetwork();
+
+    expect(neuralNetwork.andTest(inputs: [0, 0]), [0.0]);
+    expect(neuralNetwork.andTest(inputs: [1, 0]), [0.0]);
+    expect(neuralNetwork.andTest(inputs: [0, 1]), [0.0]);
+    expect(neuralNetwork.andTest(inputs: [1, 1]), [1.0]);
+  });
+
+  test('passthrough test', () async {
+    final neuralNetwork = ExampleNeuralNetwork();
+
+    expect(neuralNetwork.passthroughTest(inputs: [0, 0]), [0.0]);
+    expect(neuralNetwork.passthroughTest(inputs: [1, 0]), [1.0]);
+    expect(neuralNetwork.passthroughTest(inputs: [0, 1]), [0.0]);
+    expect(neuralNetwork.passthroughTest(inputs: [1, 1]), [1.0]);
+  });
+
+  test('not test', () async {
+    final neuralNetwork = ExampleNeuralNetwork();
+
+    expect(neuralNetwork.notTest(inputs: [0, 0]), [1.0]);
+    expect(neuralNetwork.notTest(inputs: [1, 0]), [1.0]);
+    expect(neuralNetwork.notTest(inputs: [0, 1]), [0.0]);
+    expect(neuralNetwork.notTest(inputs: [1, 1]), [0.0]);
   });
 }

a379340f1a72dfeb125c6f82b42a647bbe04e6af
Daniel Couturier <dancout@umich.edu>
2023-08-16 08:49:14 -0400

    PerceptronLayer Documentation

--- lib/models/perceptron_layer.dart
+++ lib/models/perceptron_layer.dart
@@ -1,11 +1,14 @@
 import 'package:equatable/equatable.dart';
 import 'package:neural_network_skeleton/models/perceptron.dart';
 
+/// The collection of [Perceptron] objects within a single layer of a
+/// [NeuralNetwork].
 class PerceptronLayer extends Equatable {
   const PerceptronLayer({
     required this.perceptrons,
   });
 
+  /// The collection of [Perceptron] objects within a [PerceptronLayer].
   final List<Perceptron> perceptrons;
 
   @override
@@ -15,5 +18,6 @@ class PerceptronLayer extends Equatable {
 }
 
 extension LayerExtension on PerceptronLayer {
+  /// The number of [Perceptron] objects within this [PerceptronLayer].
   int get numPerceptrons => perceptrons.length;
 }

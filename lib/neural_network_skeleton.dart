c2af16b67d57a9040f6e8f04bdd132323d83539b
Daniel Couturier <dancout@umich.edu>
2024-07-19 13:59:45 -0400

    Updating library used for vector multiplication

--- lib/neural_network_skeleton.dart
+++ lib/neural_network_skeleton.dart
@@ -2,7 +2,7 @@ library neural_network_skeleton;
 
 import 'package:equatable/equatable.dart';
 import 'package:json_annotation/json_annotation.dart';
-import 'package:ml_linalg/vector.dart';
+import 'package:matrix_utils/matrix_utils.dart';
 
 part 'neural_network_skeleton.g.dart';
 part 'package:neural_network_skeleton/models/perceptron.dart';
